939423bedf74af1a20dc135a01daa8087f4b9e2e
Edward Shao <edwardxzshao@gmail.com>
2022-11-28 18:19:50 -0500

    implemented generateMoves functionality for King, Queen. Added generateMoves to test harness

--- King.cc
+++ King.cc
@@ -9,7 +9,26 @@ using namespace std;
 King::King(bool white, pair<int, int> position) : Piece{white, 0, position, 'K'}, moved{false} {}
 
 vector<Move> King::generateMoves(){
+    vector<Move> rawList = {};
+    int x = position.first;
+    int y = position.second;
+    rawList.push_back(Move{this, make_pair(x+1, y)});
+    rawList.push_back(Move{this, make_pair(x-1, y)});
+    rawList.push_back(Move{this, make_pair(x, y+1)});
+    rawList.push_back(Move{this, make_pair(x, y-1)});
+    rawList.push_back(Move{this, make_pair(x+1, y+1)});
+    rawList.push_back(Move{this, make_pair(x+1, y-1)});
+    rawList.push_back(Move{this, make_pair(x-1, y+1)});
+    rawList.push_back(Move{this, make_pair(x-1, y-1)});
+
     vector<Move> moveList = {};
+    for(Move move : rawList){
+        int x = move.getEndPos().first;
+        int y = move.getEndPos().second;
+        if(!((x < 0) || (x > 7) || (y < 0) || (y > 7))){
+            moveList.push_back(move);
+        }
+    } 
     return moveList;
 }
 
--- Queen.cc
+++ Queen.cc
@@ -9,7 +9,29 @@ using namespace std;
 Queen::Queen(bool white, pair<int, int> position) : Piece{white, 9, position, 'Q'} {}
 
 vector<Move> Queen::generateMoves(){
+    vector<Move> rawList = {};
+    int x = position.first;
+    int y = position.second;
+    for(int i = 1; i < 8; i++){
+    rawList.push_back(Move{this, make_pair(x+i, y)});
+    rawList.push_back(Move{this, make_pair(x-i, y)});
+    rawList.push_back(Move{this, make_pair(x, y+i)});
+    rawList.push_back(Move{this, make_pair(x, y-i)});
+    rawList.push_back(Move{this, make_pair(x+i, y+i)});
+    rawList.push_back(Move{this, make_pair(x+i, y-i)});
+    rawList.push_back(Move{this, make_pair(x-i, y+i)});
+    rawList.push_back(Move{this, make_pair(x-i, y-i)});
+    }
+
+
     vector<Move> moveList = {};
+    for(Move move : rawList){
+        int x = move.getEndPos().first;
+        int y = move.getEndPos().second;
+        if(!((x < 0) || (x > 7) || (y < 0) || (y > 7))){
+            moveList.push_back(move);
+        }
+    } 
     return moveList;
 }
 
